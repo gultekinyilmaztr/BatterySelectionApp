1cd3e9c2610029cee35bf67e3e10ad1fbe4bbdac
Gültekin <gultekinyilmaz@hotmail.com.tr>
2023-11-29 14:31:15 +0300

    CategoryWithProductsDto

---
+++ Battery.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Battery.Repository.Concrete.EntityFramework;
+using CorePackages.Entities.Concrete;
+using CorePackages.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battery.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(BatterySelectionDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
+        }
+    }
+}
---
+++ CorePackages/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using CorePackages.Entities.Concrete;
+using CorePackages.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePackages.Services
+{
+    public interface ICategoryService : IService<Category>
+    {
+        public Task<CustomResponseDto<CategoryWithProductsDto>> GetSingleCategoryByIdWithProductsAsync(int categoryId);
+    }
+}
